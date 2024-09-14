fb1419da039ffa4d51b91d8e71b2464f46d9f040
Katie Petrine <kpetrine@gmail.com>
2024-09-14 16:53:02 -0700

    update code

--- form.js
+++ form.js
@@ -2,24 +2,31 @@
   'use strict';
   feather.replace({ 'aria-hidden': 'true' });
 
+  //Create categories
+  const nameInput = document.getElementById('name');
   const emailInput = document.getElementById('email');
   const commentInput = document.querySelector('#comment');
   const addButton = document.getElementById('add-comment');
   const commentsTable = document.querySelector('table > tbody');
 
+  console.log(nameInput);
   console.log(emailInput);
   console.log(commentInput);
   console.log(addButton);
   console.log(commentsTable);
 
+  // Create add button
   addButton.addEventListener('click', () => {
-    console.log("Button Clicked");
+       console.log("Button Clicked");
+    const nameText =nameInput.value;
     const emailText = emailInput.value;
     const commentsText = commentInput.value;
     
+    // Create table
     const row = createHTMLElement(`
       <tr>
-        <td>${emailText}</td>
+      <td>${nameText} </td> 
+      <td>${emailText}</td>
         <td>${commentsText}</td>
         <td><i class="bi bi-trash3-fill delete-btn"></i></td>
       </tr>
